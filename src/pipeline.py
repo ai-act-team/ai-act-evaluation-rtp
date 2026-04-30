f42bb39b1a0d0c7b6afd36caba28ef136f578176
birimabao01-cloud <birimabao01@gmail.com>
2026-04-30 22:32:41 +0200

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -14,8 +14,8 @@ from evaluation.evaluation_llm import evaluer_modele
 
 
 def main():
-    chemin_entree = "data/raw/prompts.jsonl"
-    chemin_sortie = "data/processed/rtp_propre.csv"
+    chemin_entree = r"C:\Users\USER\ai-act-evaluation-rtp\data\raw\prompts.jsonl"
+    chemin_sortie = r"C:\Users\USER\ai-act-evaluation-rtp\dataprocessed/rtp_propre.csv"
 
     # 1. Nettoyage
     df = pretraiter(chemin_entree)
