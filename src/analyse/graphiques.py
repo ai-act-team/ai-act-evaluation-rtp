5155ef2f0441b86c14cd9dc40c8c2dbe47039835
birimabao01-cloud <birimabao01@gmail.com>
2026-06-15 06:49:34 +0200

    Update graphiques.py

--- src/analyse/graphiques.py
+++ src/analyse/graphiques.py
@@ -23,7 +23,7 @@ def histogrammes(df: pd.DataFrame, colonnes=None):
 
 def boxplots(df: pd.DataFrame, colonnes=None):
     if colonnes is None:
-        colonnes = ["toxicite_prompt", "toxicite_continuation"]
+        colonnes = ["toxicite_prompt", "toxicite_reponse"]
     
     plt.figure(figsize=(7, 5))
     # On filtre seulement sur les colonnes demandées présentes dans le df
